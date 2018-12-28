08681d4e1799c64b24dfe4c00e193199100f45ce
David Votrubec <david.votrubec@gmail.com>
2018-12-28 13:13:42 +0100

    First draft of welcome screen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Container, Header, Icon, Segment, Button } from 'semantic-ui-react'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+      <React.Fragment>
+        <Header attached="top" inverted="true" size="large">Video Thumbnailer</Header>
+        <Container>
+
+          <Segment placeholder>
+            <Header icon>
+              <Icon name='video file outline' />
+              Upload short video to trigger thumbnails generation in AWS Lambda.
+              <br/>
+              When the thumbnails are generated, they will appear here.
+            </Header>
+            <Button primary>Upload (short) video</Button>
+          </Segment>
+          
+        </Container>
+      </React.Fragment>
     );
   }
 }
